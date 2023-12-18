3461e5252f0c233c3821b8038ff9081187601ca0
babel <cholodecki@gmail.com>
2023-12-18 16:31:10 +0100

    "ready" for presentation

--- physics.py
+++ physics.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 import ui_elements
 
@@ -5,20 +7,27 @@ import ui_elements
 class PhysicsCircle(ui_elements.Ellipse):
     all_physics_circles = []
 
-    def __init__(self, surface, x_cord, y_cord, radius, main_colour, x_velocity, y_velocity, is_visible=True, width=0):
-        super().__init__(surface, x_cord, y_cord, radius, radius, main_colour, is_visible, width)
+    def __init__(self, surface, x_cord, y_cord, radius, main_colour, x_velocity, y_velocity, is_visible=True, width=1):
+        
+        self.frame = ui_elements.Ellipse(surface, x_cord-width, y_cord-width, 2*(radius+width), 2*(radius+width), (255, 255, 255))
+        
+        super().__init__(surface, x_cord, y_cord, 2*radius, 2*radius, main_colour, is_visible)
 
         self.x_velocity = x_velocity
         self.y_velocity = y_velocity
 
         self.radius = radius
 
+        self.has_collided = False
+
         PhysicsCircle.all_physics_circles.append(self)
 
     def move(self):
-        print("Moved")
         self.x_cord = self.x_cord + self.x_velocity
+        self.frame.x_cord = self.frame.x_cord + self.x_velocity
+
         self.y_cord = self.y_cord + self.y_velocity
+        self.frame.y_cord = self.frame.y_cord + self.y_velocity
 
         if self.x_cord < 0 or self.x_cord+self.x_size > self.surface.x_size:
             self.x_velocity = self.x_velocity * -1
@@ -27,8 +36,28 @@ class PhysicsCircle(ui_elements.Ellipse):
             self.y_velocity = self.y_velocity * -1
 
         self.rect_update()
+        self.frame.rect_update()
+
+    def collide(self, obstacle):
+
+        self.x_velocity = -self.x_velocity
+        self.y_velocity = -self.y_velocity
+
+        obstacle.x_velocity = -obstacle.x_velocity
+        obstacle.y_velocity = -obstacle.y_velocity
+
+    @classmethod
+    def collide_check(cls):
+        for obj in cls.all_physics_circles:
+            for obstacle in cls.all_physics_circles:
+                if obj != obstacle and not obstacle.has_collided and not obj.has_collided:
+                    if obstacle.radius + obj.radius >= math.sqrt((obstacle.x_cord-obj.x_cord)**2 + (obstacle.y_cord-obj.y_cord)**2):
+                        obj.collide(obstacle)
+                        obj.has_collided = True
+                        obstacle.has_collided = True
 
     @classmethod
     def move_all(cls):
         for obj in cls.all_physics_circles:
             obj.move()
+            obj.has_collided = False
--- program.py
+++ program.py
@@ -9,11 +9,14 @@ from pygame.locals import *
 
 import physics
 
+MAX_SPEED = 5
+RADIUS = 20
+
 
 def loop_action():
 
+    physics.PhysicsCircle.collide_check()
     physics.PhysicsCircle.move_all()
-    #ball1.colour = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
 def button_handler(down, event_key, needs_shifting, is_shifting):
@@ -29,6 +32,6 @@ program_window = window.Window(1280, 720, DOUBLEBUF, assets.bg_colour, "Paint")
 moving_surface = window.Surface(program_window, 80, 80, 1120, 560, assets.bg_colour)
 border = ui_elements.Rect(moving_surface, 0, 0, 1120, 560, assets.button_colour_light, True, 2)
 
-while len(physics.PhysicsCircle.all_physics_circles) < 20:
-    new = physics.PhysicsCircle(moving_surface, random.randint(50, 1050), random.randint(50, 510), 50, assets.blue, random.randint(-10, 10), random.randint(-10, 10))
+while len(physics.PhysicsCircle.all_physics_circles) < 10:
+    new = physics.PhysicsCircle(moving_surface, random.randint(50, 1050), random.randint(50, 510), RADIUS, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), random.randint(-MAX_SPEED, MAX_SPEED), random.randint(-MAX_SPEED, MAX_SPEED))
 
